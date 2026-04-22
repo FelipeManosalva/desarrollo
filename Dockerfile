25e248874f2ae57a632b1ea50b753466da043f5c
Felipe Manosalva <manosalva1432@gmail.com>
2026-04-22 12:38:59 -0400

    prueba deploy

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,12 @@
-# Usa imagen oficial de Node
 FROM node:18
 
-# Crear carpeta app
 WORKDIR /app
 
-# Copiar archivos
 COPY package*.json ./
-
-# Instalar dependencias
 RUN npm install
 
-# Copiar resto del proyecto
 COPY . .
 
-# Exponer puerto
 EXPOSE 3000
 
-# Comando de inicio
 CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 
+// 👇 ESTO ES CLAVE EN EASY PANEL
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hola mundo con Docker 🚀</h1>");
+  res.send("<h1>Hola mundo 🚀</h1>");
 });
 
-app.listen(PORT, () => {
+// 👇 ESTO TAMBIÉN ES CLAVE
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Servidor corriendo en puerto " + PORT);
 });
