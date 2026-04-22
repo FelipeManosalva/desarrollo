ca9b94f1fffc3c70cca8f047f40112641e0e5114
Felipe Manosalva <manosalva1432@gmail.com>
2026-04-22 12:42:42 -0400

    test index

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const app = express();
 
-// 👇 ESTO ES CLAVE EN EASY PANEL
-const PORT = process.env.PORT || 3000;
+// 👇 ESTO ES LO MÁS IMPORTANTE
+const PORT = process.env.PORT || process.env.APP_PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("<h1>Hola mundo 🚀</h1>");
 });
 
-// 👇 ESTO TAMBIÉN ES CLAVE
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Servidor corriendo en puerto " + PORT);
 });
